715dd7ec8e6cb07e79dc4b5a65d6b314823380b0
alpha-rock <adityasinghsisodiyas7@gmail.com>
2020-01-09 15:31:54 +0100

    Updates

---
+++ precache-manifest.14fc8590614970ef472626912ee23a53.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73ba44fe0c5f472ea5cfc39d81c1330e",
+    "url": "/react5/index.html"
+  },
+  {
+    "revision": "2b63249e3c0ddd201e2c",
+    "url": "/react5/static/js/2.9b0a0f53.chunk.js"
+  },
+  {
+    "revision": "40fcb67bd33fa15e040c8df8a47640af",
+    "url": "/react5/static/js/2.9b0a0f53.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7e34bb2f064ab6d4714f",
+    "url": "/react5/static/js/main.44f61f3e.chunk.js"
+  },
+  {
+    "revision": "02e6a86dfb82aeba56fd",
+    "url": "/react5/static/js/runtime-main.d8244083.js"
+  }
+]);
